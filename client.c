fc1c77ecdf48f35bc99d2206bc633de0fbd1b996
Wandressa Reis <reiswandressa@gmail.com>
2023-04-17 18:00:09 -0400

    código atualizado

--- client.c
+++ client.c
@@ -1,115 +1,51 @@
-/*
-Coisas que o professor provavelmente vai questionar:
 
-OBS: Perguntei pro ChatGPT, as respostas estão abaixo de cada pergunta
+#include <stdio.h>     
+#include <stdlib.h>     
+#include <string.h>     
+#include <stdbool.h>    
+#include <sys/socket.h> 
+#include <arpa/inet.h>  
+#include <unistd.h>     
 
-1 - O que é um socket?
-    Um socket é uma interface de programação que permite a comunicação entre processos em diferentes computadores por meio da rede. Ele permite que um
-    processo em uma máquina se conecte a um processo em outra máquina e envie e receba dados através de uma conexão de rede. Em resumo, o socket é um ponto
-    final da comunicação em uma rede e pode ser usado para estabelecer conexões TCP/IP, UDP, entre outros.
+#define NULL_CHAR '\0' 
 
-2 - O que é o AF_INET e o SOCK_STREAM?
-    AF_INET é uma constante que representa o endereço da família de protocolos IPv4 usada pelo socket. "AF" significa Address Family (Família de Endereços)
-    e "INET" significa Internet Protocol (Protocolo de Internet). O AF_INET é um dos vários tipos de famílias de endereços suportados pela API de soquetes
-    e é usado para comunicação entre dispositivos em uma rede que usam o protocolo IPv4.
+#define PORT 8080               
+#define BUFFER_SIZE 2048        
+#define END_GAME "Fim de jogo!" 
 
-    SOCK_STREAM é um tipo de socket usado para comunicação orientada a conexão confiável. Ele fornece um fluxo de dados bidirecional, que é transmitido em
-    sequência e sem perda de dados ou duplicação. Essa confiabilidade é obtida por meio de um protocolo de transporte confiável, como o protocolo TCP
-    (Transmission Control Protocol).
+bool game_over = false; 
 
-    O SOCK_STREAM é adequado para aplicativos que requerem transmissão de dados confiável e sequencial, como transferência de arquivos, comunicação entre
-    cliente e servidor, etc. Ele usa o TCP como protocolo de transporte subjacente para garantir que os dados sejam entregues ao destino corretamente e
-    em ordem.
-
-3 - O que é o htons?
-    A função htons é uma função em C que converte o valor de uma porta (porta é um número inteiro que identifica um processo de rede em um host) da ordem
-    dos bytes do host para a ordem dos bytes da rede. A ordem dos bytes da rede é definida como a ordem em que os bytes são transmitidos pela rede, que é
-    a ordem de byte "big-endian". Em algumas arquiteturas de computadores, como o Intel x86, a ordem dos bytes do host é a ordem de byte "little-endian".
-
-    O uso da função htons é importante porque as diferentes arquiteturas de computadores podem armazenar valores em diferentes ordens de bytes, e a rede
-    exige uma ordem de byte específica para transmitir os dados corretamente. Portanto, a função htons é usada para garantir que os dados sejam transmitidos
-    corretamente pela rede, independentemente da arquitetura do computador.
-
-4 - O que é o inet_pton?
-    inet_pton é uma função da biblioteca de rede em C que converte um endereço IP em sua representação binária. A função recebe como entrada uma string
-    contendo um endereço IP no formato de pontos decimais (por exemplo, "192.0.2.1") e um ponteiro para um endereço em uma estrutura in_addr que conterá
-    a representação binária correspondente do endereço IP.
-
-    A função retorna um valor inteiro indicando se a conversão foi bem sucedida ou não. Se a conversão foi bem sucedida, a função retorna um valor diferente
-    de zero. Caso contrário, retorna zero.
-
-    O nome inet_pton significa "Internet address to presentation string" (endereço da Internet para string de apresentação). Essa função é uma das várias
-    funções disponíveis na biblioteca de rede em C para manipular endereços IP e outros aspectos da comunicação em rede.
-
-5 - O que a string nessa linha "scanf("%[^\n]%*c", buffer);" faz?
-    A string de formato usada aqui tem duas especificações de conversão, %[^\n] e %*c.
-
-    %[^\n]: Essa especificação de conversão instrui scanf a ler uma string de caracteres da entrada padrão até encontrar um caractere de nova linha (\n)
-    ou o final da entrada. O [^\n] diz ao scanf para combinar com qualquer caractere, exceto o caractere de nova linha, o que significa que ele lerá toda
-    a linha de entrada até o final da linha. Os caracteres lidos são armazenados no array de buffer fornecido como argumento para scanf.
-
-    %*c: Esta especificação de conversão diz ao scanf para ler um único caractere da entrada padrão e descartá-lo, sem armazená-lo em nenhuma variável.
-    O * em %*c indica que o caractere deve ser lido e descartado, mas não armazenado em nenhum lugar. Isso é necessário porque quando o scanf lê a linha
-    de entrada usando %[^\n], ele para de ler logo antes do caractere de nova linha, mas deixa o caractere de nova linha na entrada. A especificação de
-    conversão %*c consome esse caractere de nova linha, permitindo que o scanf comece a ler a próxima linha de entrada corretamente.
-
-    Portanto, colocando tudo junto, a chamada de scanf lê uma linha de entrada da entrada padrão para o array de buffer e, em seguida, descarta o caractere
-    de nova linha que o segue, deixando a entrada pronta para ler a próxima linha de entrada.Isso pode ser útil para ler linhas de texto da entrada padrão,
-    pois permite ler uma linha inteira de uma só vez, em vez de ler um caractere de cada vez.
-
-6 - O que a função strncmp faz?
-    A função strncmp é uma função da linguagem de programação C que é usada para comparar duas strings (cadeias de caracteres) em C. Ela compara os n primeiros
-    caracteres de duas strings e retorna um valor inteiro que indica se as strings são iguais, ou se a primeira string é maior ou menor que a segunda string,
-    de acordo com a ordem lexicográfica.
-*/
-
-#include <stdio.h>      // Biblioteca padrão de entrada e saída
-#include <stdlib.h>     // Biblioteca padrão de funções
-#include <string.h>     // Biblioteca padrão de manipulação de strings
-#include <stdbool.h>    // Biblioteca padrão de variáveis booleanas
-#include <sys/socket.h> // Biblioteca padrão de soquetes
-#include <arpa/inet.h>  // Biblioteca padrão de endereços de internet
-#include <unistd.h>     // Biblioteca padrão de funções de entrada e saída
-
-#define NULL_CHAR '\0' // Caractere nulo
-
-#define PORT 8080               // Porta do servidor
-#define BUFFER_SIZE 2048        // Tamanho do buffer
-#define END_GAME "Fim de jogo!" // Mensagem de fim de jogo
-
-bool game_over = false; // Variável booleana para indicar o fim do jogo
-
-int inicializar_socket();                      // Função para inicializar o socket
-void enviar_mensagem(int sock, char *buffer);  // Função para enviar mensagem
-void receber_mensagem(int sock, char *buffer); // Função para receber mensagem
+int inicializar_socket();                      
+void enviar_mensagem(int sock, char *buffer);  
+void receber_mensagem(int sock, char *buffer); 
 
 int main()
 {
-    int sock = inicializar_socket();        // Inicializando o socket
-    char buffer[BUFFER_SIZE] = {NULL_CHAR}; // Buffer para armazenar as mensagens
+    int sock = inicializar_socket();        
+    char buffer[BUFFER_SIZE] = {NULL_CHAR};
 
-    printf("Como você quer ser chamado no jogo? "); // Perguntando o nome do jogador
-    enviar_mensagem(sock, buffer);                  // Enviando o nome do jogador
+    printf("Como você quer ser chamado no jogo? "); 
+    enviar_mensagem(sock, buffer);                 
 
-    while (true) // Loop infinito
+    while (true) 
     {
-        receber_mensagem(sock, buffer); // Recebendo mensagem do servidor
+        receber_mensagem(sock, buffer); 
 
-        if (game_over) // Verificando se o jogo acabou
+        if (game_over) 
         {
-            break; // Encerrando o loop
+            break; 
         }
 
-        enviar_mensagem(sock, buffer); // Enviando mensagem para o servidor
+        enviar_mensagem(sock, buffer); 
     }
 
-    return 0; // Encerrando o programa
+    return 0; 
 }
-
+ 
 int inicializar_socket()
 {
-    int sock = 0;                            // Descritor do socket
-    struct sockaddr_in endereco_do_servidor; // Estrutura que armazena o endereço do servidor
+    int sock = 0;                          
+    struct sockaddr_in endereco_do_servidor; 
 
     sock = socket(AF_INET, SOCK_STREAM, 0); // Criando o socket
 
@@ -127,20 +63,20 @@ int inicializar_socket()
 
 void enviar_mensagem(int sock, char *buffer)
 {
-    scanf("%[^\n]%*c", buffer);             // Lendo a mensagem do usuário
-    send(sock, buffer, strlen(buffer), 0);  // Enviando a mensagem para o servidor
-    memset(buffer, NULL_CHAR, BUFFER_SIZE); // Zerando o buffer
+    scanf("%[^\n]%*c", buffer);            
+    send(sock, buffer, strlen(buffer), 0);
+    memset(buffer, NULL_CHAR, BUFFER_SIZE); 
 }
 
 void receber_mensagem(int sock, char *buffer)
 {
-    read(sock, buffer, BUFFER_SIZE); // Lendo a resposta do servidor
-    printf("%s", buffer);            // Imprimindo a resposta do servidor
+    read(sock, buffer, BUFFER_SIZE); 
+    printf("%s", buffer);            
 
-    if (strncmp(buffer + strlen(buffer) - strlen(END_GAME) - 1, END_GAME, strlen(END_GAME)) == 0) // Verificando se o jogo acabou
+    if (strncmp(buffer + strlen(buffer) - strlen(END_GAME) - 1, END_GAME, strlen(END_GAME)) == 0) 
     {
-        game_over = true; // Finalizando o jogo
+        game_over = true; 
     }
 
-    memset(buffer, NULL_CHAR, BUFFER_SIZE); // Zerando o buffer
+    memset(buffer, NULL_CHAR, BUFFER_SIZE); 
 }
--- server.c
+++ server.c
@@ -1,42 +1,42 @@
-#include <time.h>       // Biblioteca padrão de funções de tempo
-#include <stdio.h>      // Biblioteca padrão de entrada e saída
-#include <stdlib.h>     // Biblioteca padrão de funções
-#include <string.h>     // Biblioteca padrão de manipulação de strings
-#include <sys/socket.h> // Biblioteca padrão de soquetes
-#include <arpa/inet.h>  // Biblioteca padrão de endereços de internet
-#include <unistd.h>     // Biblioteca padrão de funções de entrada e saída
-#include <pthread.h>    // Biblioteca padrão de threads
-#include <stdbool.h>    // Biblioteca padrão de variáveis booleanas
+#include <time.h>       
+#include <stdio.h>      
+#include <stdlib.h>    
+#include <string.h>     
+#include <sys/socket.h> 
+#include <arpa/inet.h>  
+#include <unistd.h>     
+#include <pthread.h>    
+#include <stdbool.h>    
 
-#define NULL_CHAR '\0'  // Caractere nulo
-#define HIDDEN_CHAR '_' // Caractere para representar letras não descobertas
+#define NULL_CHAR '\0'  
+#define HIDDEN_CHAR '_' 
 
-#define PORT 8080        // Porta do servidor
-#define BUFFER_SIZE 2048 // Tamanho do buffer
-#define MAX_WORD_SIZE 50 // Tamanho máximo da palavra
+#define PORT 8080        
+#define BUFFER_SIZE 2048 
+#define MAX_WORD_SIZE 50 
 
-#define MAX_CLIENTS 2  // Número máximo de clientes
-#define MAX_ATTEMPTS 5 // Número máximo de tentativas
+#define MAX_CLIENTS 2  
+#define MAX_ATTEMPTS 5 
 
 // Client Messages
-#define MSG_GUESS "Digite uma letra: "                      // Mensagem para pedir uma letra
-#define MSG_WIN "Você ganhou! A palavra era: "              // Mensagem de vitória
-#define MSG_LOSE "Você perdeu! A palavra era: "             // Mensagem de derrota
-#define COR_GUESS "Suposição correta!"                      // Mensagem de suposição correta
-#define WRG_GUESS "Letra incorreta! Tentativas restantes: " // Mensagem de suposição incorreta
-#define END_GAME "Fim de jogo!"                             // Mensagem de fim de jogo
+#define MSG_GUESS "Digite uma letra: "                      
+#define MSG_WIN "Você ganhou! A palavra era: "              
+#define MSG_LOSE "Você perdeu! A palavra era: "             
+#define COR_GUESS "Suposição correta!"                      
+#define WRG_GUESS "Letra incorreta! Tentativas restantes: " 
+#define END_GAME "Fim de jogo!"                            
 
 char word[MAX_WORD_SIZE]; // Palavra escolhida para o jogo
 
-int new_socket[MAX_CLIENTS]; // Array de descritores de soquete para os clientes
-pthread_t tid[MAX_CLIENTS];  // Array de identificadores de threads para os clientes
-pthread_mutex_t mutex;       // Mutex para proteger o acesso à variável word
+int new_socket[MAX_CLIENTS]; 
+pthread_t tid[MAX_CLIENTS];  
+pthread_mutex_t mutex;       
 
-int inicializar_socket();                            // Função para inicializar o socket
-void *handle_guess(void *arg);                       // Função para lidar com as suposições dos clientes
-void escolher_palavra_aleatoria(char *result);       // Função para escolher uma palavra aleatória do arquivo
-void receber_mensagem(int new_socket, char *buffer); // Função para receber uma mensagem do cliente
-void enviar_mensagem(int new_socket, char *buffer);  // Função para enviar uma mensagem para o cliente
+int inicializar_socket();                           
+void *handle_guess(void *arg);                       
+void escolher_palavra_aleatoria(char *result);       
+void receber_mensagem(int new_socket, char *buffer); 
+void enviar_mensagem(int new_socket, char *buffer);  
 
 int main() // Função Principal
 {
@@ -51,10 +51,10 @@ int main() // Função Principal
 
     pthread_mutex_init(&mutex, NULL); // Inicializa o mutex
 
-    int client_count = 0;              // Contador de clientes
-    while (client_count < MAX_CLIENTS) // Enquanto o número de clientes for menor que o máximo
+    int client_count = 0;              
+    while (client_count < MAX_CLIENTS) 
     {
-        int clilen = sizeof(cli_addr);                                                               // Tamanho da estrutura de endereço de internet
+        int clilen = sizeof(cli_addr);                                                               
         new_socket[client_count] = accept(sock, (struct sockaddr *)&cli_addr, (socklen_t *)&clilen); // Aceita a conexão do cliente
 
         pthread_create(&tid[client_count], NULL, handle_guess, &new_socket[client_count]); // Cria uma thread para lidar com as suposições do cliente
@@ -76,11 +76,11 @@ void *handle_guess(void *arg)
 {
     int sock = *(int *)arg; // Descritor de soquete do cliente
 
-    char guess;                             // Letra suposta
-    char word_local[MAX_WORD_SIZE];         // Palavra escolhida para o jogo
-    int tent_restantes = MAX_ATTEMPTS;      // Número de tentativas restantes
-    char palavra_escondida[MAX_WORD_SIZE];  // Palavra escondida
-    char buffer[BUFFER_SIZE] = {NULL_CHAR}; // Buffer
+    char guess;                             
+    char word_local[MAX_WORD_SIZE];      
+    int tent_restantes = MAX_ATTEMPTS;      
+    char palavra_escondida[MAX_WORD_SIZE];  
+    char buffer[BUFFER_SIZE] = {NULL_CHAR}; 
 
     pthread_mutex_lock(&mutex);   // Bloqueia o acesso à variável word
     strcpy(word_local, word);     // Copia a palavra escolhida para o jogo
@@ -88,42 +88,41 @@ void *handle_guess(void *arg)
 
     int word_size = strlen(word_local); // Tamanho da palavra
 
-    bool game_over = false; // Variável para indicar se o jogo acabou
-    bool win = false;       // Variável para indicar se o jogador ganhou
+    bool game_over = false; 
+    bool win = false;       
 
-    memset(palavra_escondida, HIDDEN_CHAR, word_size); // Preenche a palavra escondida com o caractere de letra não descoberta
+    memset(palavra_escondida, HIDDEN_CHAR, word_size); 
     palavra_escondida[word_size] = NULL_CHAR;          // Adiciona o caractere nulo no final da palavra
 
-    read(sock, buffer, BUFFER_SIZE);                         // Lê a mensagem do cliente
-    memset(buffer, NULL_CHAR, BUFFER_SIZE);                  // Limpa o buffer
-    sprintf(buffer, "%s\n%s", palavra_escondida, MSG_GUESS); // Formata a mensagem para enviar para o cliente
+    read(sock, buffer, BUFFER_SIZE);                         
+    memset(buffer, NULL_CHAR, BUFFER_SIZE);                  
+    sprintf(buffer, "%s\n%s", palavra_escondida, MSG_GUESS); 
 
-    while (!game_over) // Enquanto o jogo não acabou
+    while (!game_over) 
     {
-        enviar_mensagem(sock, buffer); // Envia a mensagem para o cliente
-
-        receber_mensagem(sock, buffer); // Recebe a mensagem do cliente
-        guess = buffer[0];              // Pega a primeira letra da mensagem
+        enviar_mensagem(sock, buffer); 
+        receber_mensagem(sock, buffer); 
+        guess = buffer[0];              
 
         memset(buffer, NULL_CHAR, BUFFER_SIZE); // Limpa o buffer
 
-        bool found = false;                 // Variável para indicar se a letra foi encontrada
-        for (int i = 0; i < word_size; i++) // Para cada letra da palavra
+        bool found = false;                 
+        for (int i = 0; i < word_size; i++) 
         {
             bool isGuessCorrect = word_local[i] == guess; // Verifica se a letra suposta é igual a letra da palavra
 
-            if (isGuessCorrect) // Se a letra suposta for igual a letra da palavra
+            if (isGuessCorrect) 
             {
-                palavra_escondida[i] = guess; // Mostra a letra na palavra escondida
-                found = true;                 // Indica que a letra foi encontrada
+                palavra_escondida[i] = guess; 
+                found = true;                
             }
         }
 
-        if (!found) // Se a letra não foi encontrada
+        if (!found) 
         {
-            tent_restantes--;                                                                          // Decrementa o número de tentativas restantes
-            sprintf(buffer, "%s %d\n%s\n%s", WRG_GUESS, tent_restantes, palavra_escondida, MSG_GUESS); // Formata a mensagem para enviar para o cliente
-            if (tent_restantes == 0)                                                                   // Se o número de tentativas restantes for igual a 0
+            tent_restantes--;                                                                          
+            sprintf(buffer, "%s %d\n%s\n%s", WRG_GUESS, tent_restantes, palavra_escondida, MSG_GUESS); 
+            if (tent_restantes == 0)                                                                   
             {
                 game_over = true; // Indica que o jogo acabou
             }
@@ -137,26 +136,26 @@ void *handle_guess(void *arg)
 
         if (isWordRevealed) // Se a palavra foi revelada
         {
-            game_over = true; // Indica que o jogo acabou
-            win = true;       // Indica que o jogador ganhou
+            game_over = true; 
+            win = true;       
         }
     }
 
     sprintf(buffer, "%s%s\n%s\n", win ? MSG_WIN : MSG_LOSE, word_local, END_GAME); // Formata a mensagem para enviar para o cliente
 
-    enviar_mensagem(sock, buffer); // Envia a mensagem para o cliente
+    enviar_mensagem(sock, buffer); 
 
     pthread_exit(NULL); // Encerra a thread
-    close(sock);        // Fecha o soquete
+    close(sock);       
 }
 
 void escolher_palavra_aleatoria(char *result)
 {
     FILE *fp;                    // Ponteiro para o arquivo
-    char line[MAX_WORD_SIZE];    // Linha do arquivo
-    int count = 0;               // Contador de linhas
-    int random_line_number;      // Número da linha aleatória
-    int current_line_number = 0; // Número da linha atual
+    char line[MAX_WORD_SIZE];    
+    int count = 0;               
+    int random_line_number;      
+    int current_line_number = 0; 
 
     fp = fopen("./resources/palavras.txt", "r"); // Abre o arquivo
 
@@ -165,18 +164,18 @@ void escolher_palavra_aleatoria(char *result)
         count++; // Incrementa o contador de linhas
     }
 
-    srand(time(NULL));                   // Inicializa o gerador de números aleatórios
-    random_line_number = rand() % count; // Gera um número aleatório entre 0 e o número de linhas do arquivo
+    srand(time(NULL));                   
+    random_line_number = rand() % count; 
 
     rewind(fp); // Volta para o início do arquivo
 
     while (fgets(line, sizeof(line), fp) != NULL) // Enquanto não chegar ao final do arquivo
     {
-        if (current_line_number == random_line_number) // Se o número da linha atual for igual ao número da linha aleatória
+        if (current_line_number == random_line_number) 
         {
-            line[strcspn(line, "\n")] = '\0'; // Remove o caractere de nova linha
-            strcpy(result, line);             //    Copia a linha para o resultado
-            break;                            // Sai do laço
+            line[strcspn(line, "\n")] = '\0'; 
+            strcpy(result, line);             
+            break;                            
         }
         current_line_number++; // Incrementa o número da linha atual
     }
@@ -194,21 +193,21 @@ int inicializar_socket()
 
     setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt)); // Define as opções do soquete
 
-    address.sin_family = AF_INET;         // Define o domínio do soquete
-    address.sin_addr.s_addr = INADDR_ANY; // Define o endereço do soquete
-    address.sin_port = htons(PORT);       // Define a porta do soquete
+    address.sin_family = AF_INET;         
+    address.sin_addr.s_addr = INADDR_ANY; 
+    address.sin_port = htons(PORT);      
 
     bind(server_fd, (struct sockaddr *)&address, sizeof(address)); // Associa o soquete ao endereço
 
-    listen(server_fd, 3); // Define o número máximo de conexões pendentes
+    listen(server_fd, 3); 
 
-    return server_fd; // Retorna o soquete do servidor
+    return server_fd; 
 }
 
-void receber_mensagem(int new_socket, char *buffer)
+void receber_mensagem(int new_socket, char *buffer) // Recebe
 {
     read(new_socket, buffer, BUFFER_SIZE);              // Lê a mensagem do cliente
-    printf("Client %lu: %s\n", pthread_self(), buffer); // Imprime a mensagem do cliente
+    printf("Client %lu: %s\n", pthread_self(), buffer); 
 }
 
 void enviar_mensagem(int new_socket, char *buffer)
